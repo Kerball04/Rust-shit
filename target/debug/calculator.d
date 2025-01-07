a49749f028e260261648a5dd4b0ed950e5667c82
Kerball <jamesdatguy22@gmail.com>
2025-01-07 22:07:13 +0200

    Add initial build artifacts for the 'calculator' crate

---
+++ target/debug/calculator.d
@@ -0,0 +1,1 @@
+/home/kerball/git/Rust-shit/target/debug/calculator: /home/kerball/git/Rust-shit/calculator/src/main.rs
---
+++ target/debug/deps/calculator-82053bc2930a8d03.d
@@ -0,0 +1,5 @@
+/home/kerball/git/Rust-shit/target/debug/deps/calculator-82053bc2930a8d03: calculator/src/main.rs
+
+/home/kerball/git/Rust-shit/target/debug/deps/calculator-82053bc2930a8d03.d: calculator/src/main.rs
+
+calculator/src/main.rs:
