4b22b31a3fae69f988997715a4115d7f46d919e1
JamesD <james.davies@mixtelematics.com>
2025-01-09 09:57:03 +0200

    Refactor temperature conversion functionality to include a menu for user interaction and error handling

--- tempcomp/src/main.rs
+++ tempcomp/src/main.rs
@@ -1,25 +1,77 @@
 use std::io;
 
 fn main() {
-    println!("Welcome to the RTC (Rust temperature conversion)!");
+    loop {
+        println!("Temperature converter");
+        println!("1) Convert to Fahrenheit");
+        println!("2) Convert to Celsius");
+        println!("3) Exit");
 
-    // Accept user input for Fahrenheit reading
-    println!("Please enter the temperature in Fahrenheit: ");
-    let mut f = String::new();
-    io::stdin().read_line(&mut f).expect("Failed to accept user input.");
-    let fahrenheit: f64 = f.trim().parse().unwrap();
+        // read input from user
+        println!("Choose an option: ");
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Could not read input!");
 
-    // Convert Fahrenheit to Celsius
-    let celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
-    println!("The temperature in Celsius is {:.2} degrees.", celsius);
+        // convert string into u32 for comparison
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Error, invalid character entered!");
+                continue;
+            },
+        };
+        
+        match choice {
+            1 => {
+                println!("Please enter the temperature in Celsius: ");
 
-    // Accept user input for Celsius reading
-    println!("Please enter the temperature in Celsius: ");
-    let mut c = String::new();
-    io::stdin().read_line(&mut c).expect("Failed to accept user input.");
-    let celsius: f64 = c.trim().parse().unwrap();
+                // read temperature from user
+                let mut celsius = String::new();
+                io::stdin()
+                    .read_line(&mut celsius)
+                    .expect("Failed to read input");
+                let celsius: f32 = match celsius.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("This is not a valid number!");
+                        continue;
+                    },
+                };
+                    
+                println!("Converting Celsius to Fahrenheit...");
+                let fahrenheit: f32 = 9.0 / 5.0 * celsius + 32.0; 
+                println!("The temperature in Fahrenheit is {:.2}", fahrenheit);
+            },
 
-    // Convert Celsius to Fahrenheit
-    let fahrenheit = celsius * 9.0 / 5.0 + 32.0;
-    println!("The temperature in Fahrenheit is {:.2} degrees.", fahrenheit);
+            2 => {
+                println!("Please enter the temperature in Fahrenheit: ");
+
+                // read temperature from user
+                let mut fahrenheit = String::new();
+                io::stdin()
+                    .read_line(&mut fahrenheit)
+                    .expect("Failed to read input");
+                let fahrenheit: f32 = match fahrenheit.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => {
+                        println!("This is not a valid number!");
+                        continue;
+                    },
+                };
+                
+                println!("Converting Fahrenheit to Celsius...");
+                let celsius: f32 = 5.0 / 9.0 * (fahrenheit - 32.0);
+                println!("The temperature in Celsius is {:.2}", celsius);
+            },
+
+            3 => {
+                println!("Exiting...");
+                break;
+            },
+
+            _ => println!("This is not an option!")
+        }
+    }
 }
